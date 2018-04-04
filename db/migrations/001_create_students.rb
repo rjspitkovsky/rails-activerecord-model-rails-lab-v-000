6040a66abf6d43e4936991822ac99b07814dfbcd
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-04 23:30:20 +0000

    automatically backed up by learn

--- db/migrations/001_create_students.rb
+++ db/migrations/001_create_students.rb
@@ -1,8 +1,8 @@
-class CreateStudents < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
     end
   end
-end 
+end
